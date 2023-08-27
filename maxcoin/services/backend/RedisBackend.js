62f5d672e02f483256e82db1714e9f26980fd738
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-08-27 11:37:41 +0300

    replaced mongodb by redis to do the same

--- maxcoin/services/backend/RedisBackend.js
+++ maxcoin/services/backend/RedisBackend.js
@@ -1,32 +1,69 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
+const Redis = require('ioredis');
 const CoinAPI = require('../CoinAPI');
 
 class RedisBackend {
-
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.client = null;
   }
 
-  async connect() {
-
+  connect() {
+    this.client = new Redis(7379);
+    return this.client;
   }
 
   async disconnect() {
-
+    return this.client.disconnect();
   }
 
   async insert() {
-
+    // if (!this.client) throw new Error('Redis is not connected');
+    const data = await this.coinAPI.fetch();
+    const values = [];
+    data.data.entries.forEach(entry => {
+      values.push(entry[1]);
+      values.push(entry[0]);
+    });
+    return this.client.zadd('maxcoin:values', values);
   }
 
   async getMax() {
-
+    return this.client.zrange('maxcoin:values', -1, -1, 'WITHSCORES');
   }
 
   async max() {
+    console.info('Connection to Redis');
+    console.time('Redis-connect');
+    const client = this.connect();
+    if (client) {
+      console.info('Succesfully connected to Redis');
+    } else {
+      throw new Error('Connection to Redis failed');
+    }
+    console.timeEnd('Redis-connect');
 
+    console.info('Inserting into Redis');
+    console.time('Redis-insert');
+    const insertResult = await this.insert();
+    console.timeEnd('Redis-insert');
+
+    console.info(`Inserted ${insertResult} documents into Redis`);
+
+    console.info('Querying to Redis');
+    console.time('Redis-find');
+    const result = await this.getMax();
+    console.timeEnd('Redis-find');
+
+    console.info('Disconnection from Redis');
+    console.time('Redis-disconnect');
+    await this.disconnect();
+
+    console.timeEnd('Redis-disconnect');
+
+    return result;
   }
 }
 
