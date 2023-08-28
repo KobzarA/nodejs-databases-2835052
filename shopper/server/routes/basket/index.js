53d8d1d927baf8631c5ed95ec466020bdaa5fb07
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-08-28 19:42:33 +0300

    configured BasketService to store basket items info in Redis /n added added variables basketCount and currentUser /n completed shop route and started basket route

--- shopper/server/routes/basket/index.js
+++ shopper/server/routes/basket/index.js
@@ -1,18 +1,19 @@
-const express = require("express");
+const express = require('express');
+
+const BasketService = require('../../services/BasketService');
+const ItemService = require('../../services/ItemService');
+const config = require('../../config');
 
 module.exports = () => {
   const router = express.Router();
 
-  router.get("/", async (req, res) => {
-    return res.render("basket", {});
-
-    /*
+  router.get('/', async (req, res) => {
     if (!res.locals.currentUser) {
       req.session.messages.push({
-        type: "warning",
-        text: "Please log in first",
+        type: 'warning',
+        text: 'Please log in first',
       });
-      return res.redirect("/shop");
+      return res.redirect('/shop');
     }
     const basket = new BasketService(
       config.redis.client,
@@ -22,27 +23,23 @@ module.exports = () => {
     let items = [];
     if (basketItems) {
       items = await Promise.all(
-        Object.keys(basketItems).map(async (itemId) => {
+        Object.keys(basketItems).map(async itemId => {
           const item = await ItemService.getOne(itemId);
           item.quantity = basketItems[itemId];
           return item;
         })
       );
     }
-    return res.render("basket", { items });
-    */
+    return res.render('basket', { items });
   });
 
-  router.get("/remove/:itemId", async (req, res, next) => {
-    return next("Not implemented");
-
-    /*
+  router.get('/remove/:itemId', async (req, res) => {
     if (!res.locals.currentUser) {
       req.session.messages.push({
-        type: "warning",
-        text: "Please log in first",
+        type: 'warning',
+        text: 'Please log in first',
       });
-      return res.redirect("/shop");
+      return res.redirect('/shop');
     }
 
     try {
@@ -52,24 +49,23 @@ module.exports = () => {
       );
       await basket.remove(req.params.itemId);
       req.session.messages.push({
-        type: "success",
-        text: "The item was removed from the the basket",
+        type: 'success',
+        text: 'The item was removed from the the basket',
       });
     } catch (err) {
       req.session.messages.push({
-        type: "danger",
-        text: "There was an error removing the item from the basket",
+        type: 'danger',
+        text: 'There was an error removing the item from the basket',
       });
       console.error(err);
-      return res.redirect("/basket");
+      return res.redirect('/basket');
     }
 
-    return res.redirect("/basket");
-    */
+    return res.redirect('/basket');
   });
 
-  router.get("/buy", async (req, res, next) => {
-    return next("Not implemented");
+  router.get('/buy', async (req, res, next) => {
+    return next('Not implemented');
     /*
     if (!res.locals.currentUser) {
       req.session.messages.push({
